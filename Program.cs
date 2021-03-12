c2960dabfbfa619111b6fc354941e634d0f95bae
Junio da Silva Damasceno <juniowin@yahoo.com.br>
2021-03-11 22:23:26 -0300

    Recebe entrada no teclado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace entrada_usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nQual é o seu nome? ");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"\nÉ um prazer conhecê-lo {nome}!!!!");
+            
+            //O tipo "string" armazena variáveis do tipo string (textos)
+            //O comando "Console.ReadLine()" recebe entradas do usuário no teclado
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Esta é uma aplicação do tipo console
+Este programa recebe entradas do usuário no teclado (textos) a partir do console.
+
+Primeiro o programa pergunta o nome do usuário e aguarda a resposta dele no teclado. Assim que o usuário insere a resposta e aperta enter ele retorna uma mensagem para o usuário.
+
+Ex.:
+
+Qual é o seu nome? 
+Carlos
+É um prazer conhecê-lo Carlos!
